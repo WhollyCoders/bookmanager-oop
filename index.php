b1cae8e5a46f429b622f2637a100869b819a92ef
Michael L Parks <github@wmi-inc.com>
2017-07-13 13:48:34 -0400

    Initial Commit - Structure Complete

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>BookManager</title>
+  </head>
+  <body>
+    <div class="container">
+      <header>
+        <div class="header-left">
+          <h1>BookManager</h1>
+        </div>
+        <div class="header-right">
+          <nav>
+            <ul>
+              <li><a href="./">home</a></li>
+              <li><a href="./views/books">view books</a></li>
+              <li><a href="./views/authors">view authors</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <section id="content-books">
+        <h2>Welcome To BookManager</h2>
+      </section>
+      <footer>
+        <p>&copy; 2017 BookManager</p>
+      </footer>
+    </div>
+  </body>
+</html>
---
+++ views/books/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>BookManager</title>
+  </head>
+  <body>
+    <div class="container">
+      <header>
+        <div class="header-left">
+          <h1>BookManager</h1>
+        </div>
+        <div class="header-right">
+          <nav>
+            <ul>
+              <li><a href="../../index.php">home</a></li>
+              <li><a href="./">view books</a></li>
+              <li><a href="../authors">view authors</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <section id="content-books">
+        <h2>Books Index</h2>
+      </section>
+      <footer>
+        <p>&copy; 2017 BookManager</p>
+      </footer>
+    </div>
+  </body>
+</html>
